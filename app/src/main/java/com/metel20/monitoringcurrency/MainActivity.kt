d7d9dc67898e83a09b5b49159368c6f79331e974
VSadeev <metel20@gmail.com>
2024-02-26 18:05:35 +0300

    Initial commit

---
+++ app/src/main/java/com/metel20/monitoringcurrency/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.metel20.monitoringcurrency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
